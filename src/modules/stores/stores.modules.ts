7b7eb22bfb47e4857e61908fd2435a1b2f1ba600
kelvin beno <kelvinbeno526@gmail.com>
2019-07-19 13:30:25 +0300

    set up database for the stores module

---
+++ src/modules/stores/stores.modules.ts
@@ -0,0 +1,9 @@
+// import { Module } from '@nestjs/common';
+// import { StoreController } from './stores.controller';
+// import { StoreService } from './stores.services';
+// @Module({
+//     controllers: [StoreController],
+//     providers: [StoreService]
+// })
+// export class StoresModule {
+// }
