1742a0474905fc23ef7f05921c91d2589b800b19
kelvin beno <kelvinbeno526@gmail.com>
2019-07-31 12:50:07 +0300

    first commits

--- src/modules/stores/stores.modules.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { StoreController } from './stores.controller';
-// import { StoreService } from './stores.services';
-// @Module({
-//     controllers: [StoreController],
-//     providers: [StoreService]
-// })
-// export class StoresModule {
-// }
